650d5b2420b573a1e0869cda1925e632702e886b
tushar <tusharnbiswas07@gmail.com>
2026-03-17 03:25:13 +0000

    Use Case 1: Application Entry & Welcome Message

---
+++ HotelBookingApp.java
@@ -0,0 +1,22 @@
+/**
+ * Book My Stay Application
+ * Entry point of Hotel Booking System
+ *
+ * @author Student
+ * @version 1.0
+ */
+
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("      BOOK MY STAY APP           ");
+        System.out.println("      Version 1.0                ");
+        System.out.println("=================================");
+
+        System.out.println("Application Started Successfully");
+        System.out.println("Application Closed");
+
+    }
+}
